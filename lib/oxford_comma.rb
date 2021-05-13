2c132fdc233bc9847dd1aeaf6a930279580a385a
Shasha Yan <shashayan@gmail.com>
2021-05-13 16:58:36 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,5 +1,9 @@
 
-def oxford_comma(fruits)
-  fruits = ["kiwi", "durian", "starfruit", "mangos", "dragon fruits"]
-  fruits.split(",")
+def oxford_comma(arrays)
+  if array.length == 2
+    return "#{array[0]} and #{array[1]}"
+  elsif array.length > 2
+    array[-1].insert(0, "and")
+  end 
+  array.join(",")
 end
