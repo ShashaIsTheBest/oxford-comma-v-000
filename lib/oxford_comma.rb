2332501ae05bf099480794c2ab2110634925da9e
Shasha Yan <shashayan@gmail.com>
2021-05-13 17:03:05 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,9 +1,11 @@
 
 def oxford_comma(arrays)
+  if array.length == 1
+    return array
   if array.length == 2
     return "#{array[0]} and #{array[1]}"
   elsif array.length > 2
-    array[-1].insert(0, "and")
+    array[-1].insert(-2, "and")
   end
   array.join(",")
 end
